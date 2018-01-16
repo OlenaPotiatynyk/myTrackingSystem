d5c44cbd461cd5043f1fa882d069ae47bd52cee3
potiatynyk <potya_not@ukr.net>
2018-01-16 13:22:37 +0200

    Project uploaded to github

--- js/rest.js
+++ js/rest.js
@@ -1,103 +1,103 @@
 function getStatuses() {
-    return processRequest("taskStatuses", "GET");
+    return processRequest('taskStatuses', 'GET');
 }
 
 function getTaskTypes() {
-    return processRequest("taskTypes", "GET");
+    return processRequest('taskTypes', 'GET');
 }
 
 function getTaskPriorities() {
-    return processRequest("taskPriorities", "GET");
+    return processRequest('taskPriorities', 'GET');
 }
 
 function getTaskPriorityById(id) {
-    var path = "taskPriorities/" + id;
-    return processRequest(path, "GET");
+    var path = 'taskPriorities/' + id;
+    return processRequest(path, 'GET');
 }
 
 function getShortTasks() {
-    return processRequest("shortTasks", "GET");
+    return processRequest('shortTasks', 'GET');
 }
 
 function getAssignedToMeShortTasks(assigneeId) {
-    var path = "shortTasks?assigneeId=" + assigneeId;
-    return processRequest(path, "GET");
+    var path = 'shortTasks?assigneeId=' + assigneeId;
+    return processRequest(path, 'GET');
 }
 
 
 function getReportedByMeShortTasks(reporterId) {
-    var path = "shortTasks?reporterId=" + reporterId;
-    return processRequest(path, "GET");
+    var path = 'shortTasks?reporterId=' + reporterId;
+    return processRequest(path, 'GET');
 }
 
 function getHighPriorityTasks() {
-    return processRequest("shortTasks?priority=1&priority=2", "GET");
+    return processRequest('shortTasks?priority=1&priority=2', 'GET');
 }
 
 function getUsers() {
-    return processRequest("users", "GET");
+    return processRequest('users', 'GET');
 }
 
 function getUserById(id) {
-    var path = "users/" + id;
-    return processRequest(path, "GET");
+    var path = 'users/' + id;
+    return processRequest(path, 'GET');
 }
 
 
 function addUser(data) {
-    return processRequest("users", "POST", data);
+    return processRequest('users', 'POST', data);
 }
 
 function getTaskById(id) {
-    var path = "tasks/" + id + "?_embed=attachments";
-    return processRequest(path, "GET");
+    var path = 'tasks/' + id + '?_embed=attachments';
+    return processRequest(path, 'GET');
 }
 
 function getTaskComments(id) {
-    var path = "tasks/" + id + "/comments";
-    return processRequest(path, "GET");
+    var path = 'tasks/' + id + '/comments';
+    return processRequest(path, 'GET');
 }
 
 function addTaskComment(data, id) {
-    var path = "tasks/" + id + "/comments";
-    return processRequest(path, "POST", data);
+    var path = 'tasks/' + id + '/comments';
+    return processRequest(path, 'POST', data);
 }
 
 function addTaskAttachment(data, id) {
-    var path = "tasks/" + id + "/attachments";
-    return processRequest(path, "POST", data);
+    var path = 'tasks/' + id + '/attachments';
+    return processRequest(path, 'POST', data);
 }
 
 function getLastTaskNumber() {
-    return processRequest("lastTaskNumber", "GET");
+    return processRequest('lastTaskNumber', 'GET');
 }
 
 function setLastTaskNumber(id) {
     var data = {
         number: id
     };
-    return processRequest("lastTaskNumber", "PATCH", data);
+    return processRequest('lastTaskNumber', 'PATCH', data);
 }
 
 function addNewTask(data) {
-    var response = processRequest("tasks", "POST", data);
+    var response = processRequest('tasks', 'POST', data);
 
-    response = processRequest("shortTasks", "POST", generateShortTaskData(data, response));
+    response = processRequest('shortTasks', 'POST', generateShortTaskData(data, response));
 
     setLastTaskNumber(+data.taskId.replace('Task-',''));
     return response.id;
 }
 
 function updateTask(data, taskId) {
-    processRequest("tasks/" + taskId, "PATCH", data);
+    processRequest('tasks/' + taskId, 'PATCH', data);
 
-    processRequest("shortTasks/" + taskId, "PATCH", generateShortTaskData(data));
+    processRequest('shortTasks/' + taskId, 'PATCH', generateShortTaskData(data));
 }
 
 function processRequest(path, type, data) {
     return JSON.parse($.ajax({
                                  type: type,
-                                 url: "http://localhost:3000/" + path,
+                                 url: 'http://localhost:3000/' + path,
                                  data: data,
                                  async: false
                              }).responseText);
@@ -105,7 +105,7 @@ function processRequest(path, type, data) {
 
 function generateShortTaskData(data, response) {
     return {
-        id: response === undefined ? "" : response.id,
+        id: response === undefined ? '' : response.id,
         taskId: data.taskId,
         title: data.title,
         assigneeId: data.assigneeId,
